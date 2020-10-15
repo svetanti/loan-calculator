1c346f22accce9c96119ab988459864da983fdc8
Svetlana Antipova <die-chimaere@yandex.ru>
2020-10-15 10:33:44 +0300

    Updates

---
+++ precache-manifest.af64f0c29df7fb977648705461e0b8ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eef146e9f7b19375edf2117a547caca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "510f392994c7fe91e787",
+    "url": "./static/css/main.9d59ac2c.chunk.css"
+  },
+  {
+    "revision": "5a1845bafcd912c64168",
+    "url": "./static/js/2.afc2ca5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.afc2ca5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510f392994c7fe91e787",
+    "url": "./static/js/main.7c46d426.chunk.js"
+  },
+  {
+    "revision": "f22e88892d57565f944a",
+    "url": "./static/js/runtime-main.8b82071a.js"
+  },
+  {
+    "revision": "593a9f7b9d2ecea8cdf7e4ad645106a2",
+    "url": "./static/media/caret.593a9f7b.svg"
+  }
+]);
